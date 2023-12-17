3410f4f98def58a7c39f629c80ac9cf547615539
Rezvee Rahman <rezveerahman@gmail.com>
2023-12-16 22:16:39 -0500

    initial commits

---
+++ js/ButtonActivation.js
@@ -0,0 +1,28 @@
+/**
+ * @author: Rezvee Rahman
+ * 
+ * This javascript file contains code for changing the
+ * appearance of buttons. 
+ */
+
+/* Get Button Document ID */
+const MASTER_CAUTION = document.getElementById("master-caution");
+const FIRE_WARN = document.getElementById("fire-warn");
+
+// class Button {
+
+//     /**
+//      * Constructor
+//      */
+//     constructor(dom_elem) {
+//         const dom = dom_elem
+//         var active = false;
+//     }
+
+//     /**
+//      * Methods
+//      */
+//     getActive() {
+
+//     }
+// }
